d45ee71c4e18ffb3eb1fa4355c56a1e64a4763c8
Le Kiet <kietkentan@gmail.com>
2023-05-15 22:24:16 +0700

    Add Provide EventGA

--- app/src/main/java/com/khtn/zone/di/AppModule.kt
+++ app/src/main/java/com/khtn/zone/di/AppModule.kt
@@ -1,9 +1,28 @@
 package com.khtn.zone.di
 
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.installations.FirebaseInstallations
+import com.khtn.zone.utils.EventGA
+import com.khtn.zone.utils.EventGAImp
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
 @Module
-class AppModule {}
+class AppModule {
+    /*
+    @Provides
+    @Singleton
+    fun provideEventGA(
+        mAnalytics: FirebaseAnalytics,
+        mAuth: FirebaseAuth,
+        mInstallations: FirebaseInstallations
+    ): EventGA {
+        return EventGAImp(mAnalytics, mAuth, mInstallations)
+    }
+    */
+}
